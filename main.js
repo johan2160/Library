e8d961c11d9ed368d6ba9ad9e46acd8484ef5355
johandev21 <johan.p.2160@gmail.com>
2024-07-19 21:17:33 -0400

    feat: add JavaScript to infowrapper and deletewrapper divs for functionality visualization

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
 // Getting elements from the DOM
 const moon = document.querySelector("#moon");
 const sun = document.querySelector("#sun");
+
 const modal = document.querySelector("#modal");
 const addBook = document.querySelector("#add-book");
 const closeModal = document.querySelector("#close-modal-btn");
 const bookState = document.querySelectorAll(".book__state");
 
+const book = document.querySelector("#book");
+const infoWrapper = document.querySelector("#book__info-wrapper");
+const deleteWrapper = document.querySelector("#book__delete-wrapper");
+const bookDelete = document.querySelector("#book__delete");
+
+const cancelDeletion = document.querySelector("#book__cancel-deletion");
+
+// Theme toggle funcionality
 moon.addEventListener('click', () => {
   document.body.classList.add('dark-theme-variables');
   moon.style.display = 'none'
@@ -18,7 +27,7 @@ sun.addEventListener('click', () => {
   moon.style.display = 'block'
 })
 
-// Adding event listeners
+// Showing and hiding the Modal
 addBook.addEventListener('click', () => {
   modal.showModal();
 })
@@ -27,6 +36,7 @@ closeModal.addEventListener('click', () => {
   modal.close();
 })
 
+// Changing book state
 bookState.forEach(book => {
   book.addEventListener('click', () => {
 
@@ -39,3 +49,13 @@ bookState.forEach(book => {
     }
   })
 });
+
+bookDelete.addEventListener('click', () => {
+  infoWrapper.style.display = 'none';
+  deleteWrapper.style.display = 'block';
+})
+
+cancelDeletion.addEventListener('click', () => {
+  deleteWrapper.style.display = 'none';
+  infoWrapper.style.display = 'block';
+})
